6246479cce44375ae841e2baca18aac303c57255
Jason Stone <jasonstone138@gmail.com>
2025-02-23 22:06:45 -0800

    add second shader for updating cells with fluxes
    One shader previously computed the fluxes at the edges and then modified the cells accordingly, all in one pass. In preparation for a change in the flux calculation algorithm, I've split it into two passes, where first each cell computes its four fluxes and then a second shader modifies the cells according to these fluxes. This decouples the flux from one cell into another and the other cell back into the first one, both directions originally needing to be computed at once.

--- src/simulation/compute/AirShader.cpp
+++ src/simulation/compute/AirShader.cpp
@@ -13,17 +13,14 @@ AirShader::AirShader() {}
 void AirShader::init() {
 	if (initialized) return;
 
-	shader = std::move(ComputeShader(R"(
+	shader1 = std::move(ComputeShader(R"(
 #version 430
 
-// Multiplication of local sizes <= 1024 for most graphics cards
-// Larger = better since better locality but I am lazy
-layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+// 16x16x1 has good performance on my laptop's GTX 1650
+layout (local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 
-// Yes you can combine the two but memory ordering on different graphics
-// cards (cough AMD) is stupid so arrays are often padded so each element is 16 bytes
-layout(std430, binding = 0) writeonly restrict buffer DataOutStruct {
-	float dataOut[];
+layout(std430, binding = 0) writeonly restrict buffer DataFluxStruct {
+	vec4 dataFlux[];
 };
 
 layout(std430, binding = 1) readonly restrict buffer DataInStruct {
@@ -51,6 +48,138 @@ struct CellData {
 
 const float dt = 0.1;
 const vec4 diffusion = vec4(1.0) * 0.1;
+
+CellData getCell(ivec2 pos) {
+	CellData data = CellData(0.0, 0.0, 0.0, 0.0, false);
+	int x = pos.x;
+	int y = pos.y;
+
+	if (x >= 0 && x < XCELLS && y >= 0 && y < YCELLS) {
+		data.vx = dataIn[(y * XCELLS + x) * dataSize + vxOffset];
+		data.vy = dataIn[(y * XCELLS + x) * dataSize + vyOffset];
+		data.pv = dataIn[(y * XCELLS + x) * dataSize + pvOffset];
+		data.hv = dataIn[(y * XCELLS + x) * dataSize + hvOffset];
+		data.wall = dataIn[(y * XCELLS + x) * dataSize + wallOffset] != 0.0;
+	}
+
+	data.pv = clamp(data.pv + 10.1, 0.1, 266.1);
+
+	return data;
+}
+
+vec4 toVec4(CellData data) {
+	return vec4(data.vx, data.vy, data.pv, data.hv);
+}
+
+CellData toCellData(vec4 data, bool wall) {
+	return CellData(data.x, data.y, data.z, data.w, wall);
+}
+
+vec4 getFluxH(vec4 cell) {
+	vec4 momentumFlux = vec4(cell.z, 0.0, 0.0, 0.0);
+
+	vec4 advectionFlux = cell * cell.x / cell.z;
+
+	return momentumFlux + advectionFlux;
+}
+
+vec4 computeEdgeFluxH(vec4 leftCell, vec4 rightCell) {
+	vec2 averageVelocity = (leftCell.xy / leftCell.z + rightCell.xy / rightCell.z) * 0.5;
+	float averageDensity = sqrt(leftCell.z * rightCell.z);
+
+	vec4 average = vec4(averageVelocity * averageDensity, averageDensity, 0.0);
+	average += dt * (getFluxH(leftCell) - getFluxH(rightCell)) * 0.5;
+
+	average.z = max(average.z, 0.1);
+
+	vec4 diffusionFlux = (leftCell - rightCell) * diffusion;//(leftCell - rightCell) * (1.0 + length(average.xy / average.z)) * 0.5;
+
+	return diffusionFlux + getFluxH(average);//momentumFlux + advectionFlux;
+}
+
+vec4 computeEdgeFluxV(vec4 lowerCell, vec4 upperCell) {
+	return computeEdgeFluxH(lowerCell.yxzw, upperCell.yxzw).yxzw;
+}
+
+void main() {
+	ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
+	int x = pos.x;
+	int y = pos.y;
+
+	if (x >= XCELLS || y >= YCELLS) {
+		return;
+	}
+
+	vec4 upperFlux = vec4(0);
+	vec4 lowerFlux = vec4(0);
+	vec4 leftFlux = vec4(0);
+	vec4 rightFlux = vec4(0);
+
+	CellData thisCellData = getCell(pos);
+
+	if (!thisCellData.wall) {
+		vec4 thisCell = toVec4(thisCellData);
+
+		CellData upperCellData = getCell(pos + ivec2( 0, -1));
+		CellData lowerCellData = getCell(pos + ivec2( 0,  1));
+		CellData leftCellData  = getCell(pos + ivec2(-1,  0));
+		CellData rightCellData = getCell(pos + ivec2( 1,  0));
+
+		vec4 upperCell = upperCellData.wall ? thisCell : toVec4(upperCellData);
+		vec4 lowerCell = lowerCellData.wall ? thisCell : toVec4(lowerCellData);
+		vec4 leftCell  = leftCellData.wall  ? thisCell : toVec4(leftCellData);
+		vec4 rightCell = rightCellData.wall ? thisCell : toVec4(rightCellData);
+
+		upperFlux = computeEdgeFluxV(thisCell , upperCell);
+		lowerFlux = computeEdgeFluxV(lowerCell, thisCell );
+		leftFlux  = computeEdgeFluxH(leftCell , thisCell );
+		rightFlux = computeEdgeFluxH(thisCell , rightCell);
+	}
+
+	dataFlux[(y * XCELLS + x) * 4 + 0] = upperFlux * 0.5;
+	dataFlux[(y * XCELLS + x) * 4 + 1] = lowerFlux * 0.5;
+	dataFlux[(y * XCELLS + x) * 4 + 2] = leftFlux * 0.5;
+	dataFlux[(y * XCELLS + x) * 4 + 3] = rightFlux * 0.5;
+}
+	)"));
+
+	shader2 = std::move(ComputeShader(R"(
+#version 430
+
+layout (local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
+
+layout(std430, binding = 0) writeonly restrict buffer DataOutStruct {
+	float dataOut[];
+};
+
+layout(std430, binding = 1) readonly restrict buffer DataInStruct {
+	float dataIn[];
+};
+
+layout(std430, binding = 2) readonly restrict buffer DataFluxStruct {
+	vec4 dataFlux[];
+};
+
+layout(std430, binding = 3) readonly restrict buffer ConfigStruct {
+	uint XCELLS;
+	uint YCELLS;
+	uint dataSize;
+	uint vxOffset;
+	uint vyOffset;
+	uint pvOffset;
+	uint hvOffset;
+	uint wallOffset;
+};
+
+struct CellData {
+	float vx;
+	float vy;
+	float pv;
+	float hv;
+	bool wall;
+};
+
+const float dt = 0.1;
 
 CellData getCell(ivec2 pos) {
 	CellData data = CellData(0.0, 0.0, 0.0, 0.0, false);
@@ -115,53 +244,77 @@ CellData toCellData(vec4 data, bool wall) {
 	return CellData(data.x, data.y, data.z, data.w, wall);
 }
 
-vec4 getFluxH(vec4 cell) {
-	vec4 momentumFlux = vec4(cell.z, 0.0, 0.0, 0.0);
-
-	vec4 advectionFlux = cell * cell.x / cell.z;
-
-	return momentumFlux + advectionFlux;
-}
-
-vec4 computeEdgeFluxH(vec4 leftCell, vec4 rightCell) {
-	vec2 averageVelocity = (leftCell.xy / leftCell.z + rightCell.xy / rightCell.z) * 0.5;
-	float averageDensity = sqrt(leftCell.z * rightCell.z);
-
-	vec4 average = vec4(averageVelocity * averageDensity, averageDensity, 0.0);
-	average += dt * (getFluxH(leftCell) - getFluxH(rightCell)) * 0.5;
-
-	average.z = max(average.z, 0.1);
-
-	vec4 diffusionFlux = (leftCell - rightCell) * diffusion;//(leftCell - rightCell) * (1.0 + length(average.xy / average.z)) * 0.5;
-
-	return diffusionFlux + getFluxH(average);//momentumFlux + advectionFlux;
-}
-
-vec4 computeEdgeFluxV(vec4 lowerCell, vec4 upperCell) {
-	return computeEdgeFluxH(lowerCell.yxzw, upperCell.yxzw).yxzw;
-}
-
 void main() {
-	ivec2 pos = ivec2(gl_GlobalInvocationID.x, gl_GlobalInvocationID.y);
+	ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
+	int x = pos.x;
+	int y = pos.y;
+
+	if (x >= XCELLS || y >= YCELLS) {
+		return;
+	}
 
 	CellData thisCellData = getCell(pos);
 	vec4 thisCell = toVec4(thisCellData);
 
 	if (!thisCellData.wall) {
-		CellData upperCellData = getCell(pos + ivec2( 0, -1));
-		CellData lowerCellData = getCell(pos + ivec2( 0,  1));
-		CellData leftCellData  = getCell(pos + ivec2(-1,  0));
-		CellData rightCellData = getCell(pos + ivec2( 1,  0));
+		int upperY = y - 1;
+		int lowerY = y + 1;
+		int leftX  = x - 1;
+		int rightX = x + 1;
 
-		vec4 upperCell = upperCellData.wall ? thisCell : toVec4(upperCellData);
-		vec4 lowerCell = lowerCellData.wall ? thisCell : toVec4(lowerCellData);
-		vec4 leftCell  = leftCellData.wall  ? thisCell : toVec4(leftCellData);
-		vec4 rightCell = rightCellData.wall ? thisCell : toVec4(rightCellData);
+		if (upperY == -1) {
+			upperY++;
+		}
 
-		vec4 upperFlux = computeEdgeFluxV(thisCell , upperCell);
-		vec4 lowerFlux = computeEdgeFluxV(lowerCell, thisCell );
-		vec4 leftFlux  = computeEdgeFluxH(leftCell , thisCell );
-		vec4 rightFlux = computeEdgeFluxH(thisCell , rightCell);
+		if (lowerY == YCELLS) {
+			lowerY--;
+		}
+
+		if (leftX == -1) {
+			leftX++;
+		}
+
+		if (rightX == XCELLS) {
+			rightX--;
+		}
+
+		if (x >= XCELLS || y >= YCELLS) {
+			return;
+		}
+
+		CellData upperCellData = getCell(ivec2(x     , upperY));
+		CellData lowerCellData = getCell(ivec2(x     , lowerY));
+		CellData leftCellData  = getCell(ivec2(leftX , y     ));
+		CellData rightCellData = getCell(ivec2(rightX, y     ));
+
+		vec4 upperFlux = dataFlux[(y * XCELLS + x) * 4 + 0];
+		vec4 lowerFlux = dataFlux[(y * XCELLS + x) * 4 + 1];
+		vec4 leftFlux  = dataFlux[(y * XCELLS + x) * 4 + 2];
+		vec4 rightFlux = dataFlux[(y * XCELLS + x) * 4 + 3];
+
+		if (!upperCellData.wall) {
+			upperFlux += dataFlux[(upperY * XCELLS + x) * 4 + 1];
+		} else {
+			upperFlux *= 2.0;
+		}
+
+		if (!lowerCellData.wall) {
+			lowerFlux += dataFlux[(lowerY * XCELLS + x) * 4 + 0];
+		} else {
+			lowerFlux *= 2.0;
+		}
+
+		if (!leftCellData.wall) {
+			leftFlux += dataFlux[(y * XCELLS + leftX) * 4 + 3];
+		} else {
+			leftFlux *= 2.0;
+		}
+
+		if (!rightCellData.wall) {
+			rightFlux += dataFlux[(y * XCELLS + rightX) * 4 + 2];
+		} else {
+			rightFlux *= 2.0;
+		}
 
 		thisCell += (leftFlux + lowerFlux - rightFlux - upperFlux) * dt;
 	}
@@ -169,9 +322,18 @@ void main() {
 	setCell(pos, toCellData(thisCell, thisCellData.wall));
 }
 	)"));
+
+	glGenBuffers(1, &ssbo_in);
+	glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo_in);
+	glBufferData(GL_SHADER_STORAGE_BUFFER, CELL_BUFFER_SIZE, NULL, GL_DYNAMIC_DRAW);
+
+	glGenBuffers(1, &ssbo_flux);
+	glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo_flux);
+	glBufferData(GL_SHADER_STORAGE_BUFFER, FLUX_BUFFER_SIZE, NULL, GL_DYNAMIC_DRAW);
+
 	glGenBuffers(1, &ssbo_out);
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo_out);
-	glBufferData(GL_SHADER_STORAGE_BUFFER, BUFFER_SIZE, NULL, GL_DYNAMIC_DRAW);
+	glBufferData(GL_SHADER_STORAGE_BUFFER, CELL_BUFFER_SIZE, NULL, GL_DYNAMIC_DRAW);
 
 	config.XCELLS = XCELLS;
 	config.YCELLS = YCELLS;
@@ -180,11 +342,6 @@ void main() {
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo_config);
 	glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(ConfigStruct), &config, GL_STATIC_DRAW);
 	// Use glBufferSubData to update existing data it's faster
-
-	glGenBuffers(1, &ssbo_in);
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo_in);
-	CellData in[XCELLS * YCELLS] = {{1, 0, 0, 0}, {2, 0, 0, 0}, {3, 0, 0, 0}, {4, 0, 0, 0}, {5, 0, 0, 0}};
-	glBufferData(GL_SHADER_STORAGE_BUFFER, BUFFER_SIZE, &in, GL_DYNAMIC_DRAW);
 
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 	initialized = true;
@@ -208,30 +365,36 @@ void AirShader::upload(Air * air) {
 	}
 
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo_in);
-	glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, BUFFER_SIZE, &tmp_buf[0]);
+	glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, CELL_BUFFER_SIZE, &tmp_buf[0]);
 }
 
 void AirShader::run(int repetitions) {
-	shader.enable();
-
 	for (int i = 0; i < repetitions; i++) {
-		glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT); // possibly unnecessary, just using this for good measure
+		glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
 
-		// For multiple ssbos you can bind them to different indices and
-		// set the index in the compute shader code (here ssbo_out is set to index 0
-		// (as is set in the compute shader code))
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo_out); // Index, id
+		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo_flux); // Index, id
 		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, ssbo_in); // Index, id
 		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, ssbo_config); // Index, id
 
-		shader.dispatch(XCELLS, YCELLS, 1);
+		shader1.enable();
+		shader1.dispatch((XCELLS - 1) / 16 + 1, (YCELLS - 1) / 16 + 1, 1);
+		shader1.disable();
+
+		glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+
+		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo_out); // Index, id
+		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, ssbo_in); // Index, id
+		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, ssbo_flux); // Index, id
+		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 3, ssbo_config); // Index, id
+
+		shader2.enable();
+		shader2.dispatch((XCELLS - 1) / 16 + 1, (YCELLS - 1) / 16 + 1, 1);
+		shader2.disable();
 
 		auto temp = ssbo_out;
 		ssbo_out = ssbo_in;
 		ssbo_in = temp;
 	}
-
-	shader.disable();
 }
 
 void AirShader::download(Air * air) {
@@ -239,7 +402,7 @@ void AirShader::download(Air * air) {
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo_in);
 
 	// Read ssbo into tmp_buf
-	glGetBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, BUFFER_SIZE, &tmp_buf[0]);
+	glGetBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, CELL_BUFFER_SIZE, &tmp_buf[0]);
 
 	for (int y = 0; y < YCELLS; y++) {
 		for (int x = 0; x < XCELLS; x++) {
--- src/simulation/compute/AirShader.h
+++ src/simulation/compute/AirShader.h
@@ -13,6 +13,13 @@ struct CellData {
 	float wall; // ought to be bool but the shader expects everything to be floats
 };
 
+struct Vec4 {
+	float x;
+	float y;
+	float z;
+	float w;
+};
+
 struct ConfigStruct {
     unsigned int XCELLS = XCELLS;
     unsigned int YCELLS = YCELLS;
@@ -24,7 +31,8 @@ struct ConfigStruct {
 	unsigned int wallOffset = offsetof(CellData, wall) / sizeof(float);
 };
 
-inline constexpr std::size_t BUFFER_SIZE = XCELLS * YCELLS * sizeof(CellData);
+inline constexpr std::size_t CELL_BUFFER_SIZE = XCELLS * YCELLS * sizeof(CellData);
+inline constexpr std::size_t FLUX_BUFFER_SIZE = XCELLS * YCELLS * 4 * sizeof(Vec4);
 
 class Air;
 class AirShader {
@@ -42,9 +50,10 @@ public:
 
 private:
     bool initialized = false;
-    ComputeShader shader;
+    ComputeShader shader1;
+	ComputeShader shader2;
     ConfigStruct config;
-    unsigned int ssbo_out, ssbo_in, ssbo_config;
+    unsigned int ssbo_out, ssbo_flux, ssbo_in, ssbo_config;
     CellData tmp_buf[XCELLS * YCELLS];
 };
 
